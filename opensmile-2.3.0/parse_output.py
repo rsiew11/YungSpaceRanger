96daba629bcbdb9d43e9f7e93f935c65a938dbed
Namrita Murali <npmurali@andrew.cmu.edu>
2017-10-29 18:45:19 -0400

    parsing stuff

--- opensmile-2.3.0/parse_output.py
+++ opensmile-2.3.0/parse_output.py
@@ -1,10 +1,11 @@
 import csv
 import os
 import numpy as np
+import pandas
 
 output_directory = "/afs/andrew.cmu.edu/usr23/npmurali/private/18500/YungSpaceRanger/opensmile-2.3.0/soccerfield/output"
 coefficient_names = []
-format_values = ['f8']*39
+
 
 def create_average_file(filename):
     avg_dict = dict()
@@ -28,26 +29,46 @@ def avg_values_across_dict(avg_dict):
         avg_dict[key] = sum(value_array)/len(value_array)
     return avg_dict
 
-def create_classifier(avg_dict):
-    names = ['coefficient', 'value']
-    format_values = ['U25', 'f8']
-    dtype = dict(names = names, formats=format_values)
-    array = np.array(list(avg_dict.items()), dtype=dtype)
-    print repr(array)
-
-              
-
+def dict_to_value_array(mydict):
+    result = []
+    keylist = mydict.keys()
+    keylist.sort()
+    for key in keylist:
+        result.append(mydict[key])
+    return result
                 
+def add_to_final_csv(filename, avg_dict):
+    with open('allfiles_output.csv', 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in avg_dict.items():
+            writer.writerow([key, value])
+
+
 def parsefiles():
-    for filename in os.listdir(output_directory):
-        print "file looking at"+str(os.path.join(output_directory, filename))
-        full_path = os.path.join(output_directory, filename)
-        avg_dict = create_average_file(full_path)
-        avg_dict = avg_values_across_dict(avg_dict)
-        create_classifier(avg_dict)
+    header_added = False
+    with open('all_output.csv', 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for filename in os.listdir(output_directory):
+            print "file looking at"+str(os.path.join(output_directory, filename))
+            full_path = os.path.join(output_directory, filename)
+            avg_dict = create_average_file(full_path)
+            avg_dict = avg_values_across_dict(avg_dict)
+            avg_dict['name'] = filename
+            if (not(header_added)):
+                coefficient_names.remove('frameTime')
+                writer.writerow(coefficient_names)
+                header_added = True
+            writer.writerow(dict_to_value_array(avg_dict))
+    return csv_file
+
+def classify():
+    output_csv = parsefiles()
+    df = pandas.read_csv('all_output.csv')
+    print df
+
 
 
 
 if __name__ == "__main__":
-    parsefiles()
+    classify()
 
