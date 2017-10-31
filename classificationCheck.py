8416f05f46c9eea07990b20a6a661d1994c0775b
rsiew11 <rsiew11@gmail.com>
2017-10-30 22:04:20 -0400

    Update classificationCheck.py

--- classificationCheck.py
+++ classificationCheck.py
@@ -1,26 +1,19 @@
-import sys
 
+def classify(csv,wav):
+    fd = open(str(csv),"r+")
+    data = fd.readlines()
+    fd.close()
+    
+    numFiles = len(data)
 
-#use this file by running "python THISFILE.py SMTH.csv SMTH.wav"
-#sys.argv[1] is the csv file with the classification
-#sys.argv[2] is the wav file in question
+    findyDict = {}
+    for i in xrange(numFiles):
+        comma = data[i].find(',')
+        key = data[i][0:comma]
+        val = int(data[i][comma+1])
+        findyDict[key] = val
 
-
-fd = open(str(sys.argv[1]),"r+")
-
-data = fd.readlines()
-fd.close()
-numFiles = len(data)
-
-
-findyDict = {}
-for i in xrange(numFiles):
-    comma = data[i].find(',')
-    key = data[i][0:comma]
-    val = int(data[i][comma+1])
-    findyDict[key] = val
-
-print findyDict[str(sys.argv[2])]
+    return findyDict[str(wav)]
 
 
 
