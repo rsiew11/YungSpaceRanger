e1c9451ee9704838701a8bdcd3a2f4c039600e05
rsiew11 <rsiew11@gmail.com>
2017-10-30 22:00:12 -0400

    Update classificationCheck.py

--- classificationCheck.py
+++ classificationCheck.py
@@ -6,20 +6,28 @@ import sys
 #sys.argv[2] is the wav file in question
 
 
-fd = open(str(sys.argv[1]),r+)
+fd = open(str(sys.argv[1]),"r+")
 
 data = fd.readlines()
+fd.close()
 numFiles = len(data)
 
 
 findyDict = {}
 for i in xrange(numFiles):
-    comma = str.find(',')
+    comma = data[i].find(',')
     key = data[i][0:comma]
     val = int(data[i][comma+1])
     findyDict[key] = val
 
-return findyDict[str(sys.argv[2])]
+print findyDict[str(sys.argv[2])]
+
+
+
+
+
+
+
 
 
 
