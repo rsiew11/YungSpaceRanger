085de496792819c0ea422ab497fe165d60c7076d
rsiew <rsiew11@gmail.com>
2017-11-18 02:08:56 +0000

    transfering the zip file works now

--- hardware/serverFileTransfer.py
+++ hardware/serverFileTransfer.py
@@ -2,9 +2,9 @@
 
 import socket                   # Import socket module
 
-port = 50000                    # Reserve a port for your service.
+port = 60000                    # Reserve a port for your service.
 s = socket.socket()             # Create a socket object
-host = '128.237.251.59'     # Get local machine name
+host = '128.237.238.124'     # Get local machine name
 s.bind((host, port))            # Bind to the port
 s.listen(5)                     # Now wait for client connection.
 
@@ -16,7 +16,7 @@ while True:
     data = conn.recv(1024)
     print('Server received', repr(data))
     for i in xrange(1):
-        filename='home/pi/YungSpaceRanger/hardware/packet.tar'.format(i)
+        filename='pac.zip'.format(i)
         f = open(filename,'rb')
         l = f.read(1024)
         while (l):
@@ -26,6 +26,6 @@ while True:
         f.close()
 
     print('Done sending')
-    conn.send('Thank you for connecting')
+    #conn.send('Thank you for connecting')
     conn.close()
 
