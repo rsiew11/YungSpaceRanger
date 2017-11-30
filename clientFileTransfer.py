0d28b073092af5ad63977200a3d66ad562aadd43
rsiew11 <rsiew11@gmail.com>
2017-11-29 23:08:46 -0500

    adding clientFiletransfer.py

--- clientFileTransfer.py
+++ clientFileTransfer.py
@@ -1,13 +1,13 @@
 import socket                   # Import socket module
 
 s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-port = 60000                    # Reserve a port for your service.
+host = '128.237.244.81'     # Get local machine name
+port = 60001                    # Reserve a port for your service.
 
 s.connect((host, port))
 s.send("Hello server!")
 
-with open('received_file', 'wb') as f:
+with open('pac.zip', 'wb') as f:
     print 'file opened'
     while True:
         print('receiving data...')
@@ -19,6 +19,6 @@ with open('received_file', 'wb') as f:
         f.write(data)
 
 f.close()
-print('Successfully get the file')
+print('Successfully got the file')
 s.close()
 print('connection closed')
